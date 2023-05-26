0541da97bb746fcf71c86158b205501613f0c24f
Riccardo Agatea <riccardo.agatea@gmail.com>
2023-05-26 13:15:25 +0200

    update default gene

--- vendor/assets/default_gene.rb
+++ vendor/assets/default_gene.rb
@@ -1,15 +1,3 @@
-executable :gem do
-  command :install
-end
+gem :install, :rails
 
-executable :rails do
-  command :new do
-    flag :asset_pipeline
-  end
-end
-
-gem.install :rails
-
-rails.new project_name do
-  asset_pipeline false
-end
+rails :new, {asset_pipeline: false}, project_name
